1697b644359e10377bc6b951045cd8c74902d6a9
Joao <joaojacomedev@gmail.com>
2024-05-05 00:22:57 -0500

    set pending todos counter

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,12 +1,13 @@
 import html from './app.html?raw';
 import todoStore, { Filters } from '../store/todo.store'
-import { renderTodos } from './use-cases';
+import { renderPendingTodos, renderTodos } from './use-cases';
 
 const ElementIDs = {
     ClearCompleted: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
-    TodoFilters: '.filtro'
+    TodoFilters: '.filtro',
+    PendingCountLabel : '#pending-count'
 }
 
 /**
@@ -19,6 +20,11 @@ export const App = (elementId) => {
     const displayTodos = () => {
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
         renderTodos(ElementIDs.TodoList, todos);
+        updatePendingCount();
+    }
+
+    const updatePendingCount = () => {
+        renderPendingTodos( ElementIDs.PendingCountLabel );
     }
 
     (() => {
--- src/todos/use-cases/index.js
+++ src/todos/use-cases/index.js
@@ -1,2 +1,4 @@
+export { renderPendingTodos } from "./rending-pending";
 export { createTodoHtml } from "./create-todo-html";
 export { renderTodos } from "./render-todos";
+
