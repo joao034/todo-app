13e7cb9eac879da3e94c4cbb19a372d16fb00aee
Joao <joaojacomedev@gmail.com>
2024-05-04 21:08:18 -0500

    toggle to do

--- src/todos/app.js
+++ src/todos/app.js
@@ -3,7 +3,8 @@ import todoStore from '../store/todo.store'
 import { renderTodos } from './use-cases';
 
 const ElementIDs = {
-    TodoList: '.todo-list'
+    TodoList: '.todo-list',
+    NewTodoInput : '#new-todo-input'
 }
 
 /**
@@ -24,5 +25,26 @@ export const App = ( elementId ) => {
         document.querySelector( elementId ).append( app );
         displayTodos();
     })();
+
+    //HTML references
+    const newDesciptionInput = document.querySelector( ElementIDs.NewTodoInput );
+    const itemUl = document.querySelector( ElementIDs.TodoList );
+
+    //Listeners
+    newDesciptionInput.addEventListener( 'keyup', (event) => {
+        if ( event.keyCode !== 13 ) return;
+        if ( event.target.value.trim().length === 0 ) return;
+        
+        todoStore.addTodo ( event.target.value );
+        displayTodos();
+
+        event.target.value = ''
+    });
+
+    itemUl.addEventListener( 'click', (event) => {
+        const item =  event.target.closest('[data-id]');
+        todoStore.toggleTodo( item.getAttribute('data-id'));
+        displayTodos()
+    });
 
 }
